12dd00b04659cf38392b316510375eda12511376
dasechh <kosarev-2004@mail.ru>
2026-03-30 22:16:00 +0300

    template dependencies deletion

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../pages/welcome/welcome";
+import Home from "@pages/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "NeoBank" },
+    { name: "description", content: "Welcome to NeoBank website!" },
   ];
 }
 
-export default function Home() {
-  return <Welcome />;
+export default function homePage() {
+  return <Home />;
 }

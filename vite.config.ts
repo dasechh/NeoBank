fbb718fe2cb38fa66f53a3353df5709d44cabc25
dasechh <kosarev-2004@mail.ru>
2026-04-10 01:52:51 +0300

    node types fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
   plugins: [react()],
   server: {

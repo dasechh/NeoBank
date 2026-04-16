484d2b4e7fab47c8f3f98b73c734cae4b84debfb
dasechh <kosarev-2004@mail.ru>
2026-04-16 15:35:52 +0300

    removed slider constants dependency

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,4 +5,3 @@ export {
   newsApiUrl,
 } from './api.constants';
 export { msInMinute, msIn15Minutes } from './time.constants';
-export { cardSize, sliderGap, scrollAmount } from './slider.constants';
--- src/constants/slider.constants.ts
+++
@@ -1,3 +0,0 @@
-export const cardSize: number = 320;
-export const sliderGap: number = 80;
-export const scrollAmount: number = cardSize + sliderGap;

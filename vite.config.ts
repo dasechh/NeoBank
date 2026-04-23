98363bc026f90b0adf9c44486aba054cc2493aac
dasechh <kosarev-2004@mail.ru>
2026-04-23 22:37:49 +0300

    added api endpoints

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { apiBaseUrl } from './src/constants';
 
 const __dirname = path.dirname(__filename);
 
@@ -9,6 +10,24 @@ export default defineConfig({
   plugins: [react(), svgr()],
   server: {
     open: true,
+    proxy: {
+      '/email': {
+        target: `${apiBaseUrl}`,
+        changeOrigin: true,
+      },
+      '/application': {
+        target: `${apiBaseUrl}`,
+        changeOrigin: true,
+      },
+      '/document': {
+        target: `${apiBaseUrl}`,
+        changeOrigin: true,
+      },
+      '/admin': {
+        target: `${apiBaseUrl}`,
+        changeOrigin: true,
+      },
+    },
   },
   resolve: {
     alias: {
